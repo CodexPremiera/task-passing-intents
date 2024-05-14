30dfaf0329f5986d5f0af59a885d75f8dd2c7cd2
CodexPremiera <ashleyken.professional@gmail.com>
2024-05-15 00:31:14 +0800

    complete registration page

--- app/src/main/java/task/intent/IntentsActivity.java
+++ app/src/main/java/task/intent/IntentsActivity.java
@@ -1,14 +1,24 @@
 package task.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 
 public class IntentsActivity extends AppCompatActivity {
 
+    private void initializeWindow() {
+        int dark_blue = ContextCompat.getColor(this, R.color.intents_dark_blue);
+        getWindow().setNavigationBarColor(dark_blue);
+        getWindow().setStatusBarColor(dark_blue);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // setup the navigation bar and status bar
+        this.initializeWindow();
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.intents_main);
     }
 }
